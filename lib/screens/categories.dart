888b9d4180d200372e760f50b8b0776dd75696fd
CodeLaboratory-Patrick <gptnc5100@gmail.com>
2024-12-03 13:22:29 +0100

    Passing data to the Target Screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 import 'package:meals/screens/meals.dart';
+import 'package:meals/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     // = Navigator.push(context, route);
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
-          title: 'Some title',
-          meals: [],
+          title: category.title,
+          meals: filteredMeals,
         ),
-    ),
+      ),
     );
   }
 
@@ -27,17 +33,20 @@ class CategoriesScreen extends StatelessWidget {
       body: GridView(
         padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, 
+          crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
-          ),
+        ),
         children: [
           //availableCategories.map((category) => CategoryGridItem(category: category)).toList() {<- alternative}
-         for (final category in availableCategories)
-         CategoryGridItem(category: category, onSelectCategory: () {
-          _selectCategory(context);
-         },)
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
