7ebc3f720bf1636fae7d6ba02a944620ef449c34
CodeLaboratory-Patrick <gptnc5100@gmail.com>
2024-12-05 10:59:48 +0100

    Adding Navigation to the MealDetails Screen.

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
-import 'package:meals/models/meal.dart';
+
 import 'package:meals/widgets/meal_item_trait.dart';
+import 'package:meals/models/meal.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({
     super.key,
     required this.meal,
+    required this.onSelectMeal,
   });
 
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
 
   String get complexityText {
     return meal.complexity.name[0].toUpperCase() +
@@ -31,7 +34,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
@@ -44,7 +49,7 @@ class MealItem extends StatelessWidget {
             Positioned(
               bottom: 0,
               left: 0,
-              right: 50,
+              right: 0,
               child: Container(
                 color: Colors.black54,
                 padding:
@@ -56,11 +61,12 @@ class MealItem extends StatelessWidget {
                       maxLines: 2,
                       textAlign: TextAlign.center,
                       softWrap: true,
-                      overflow: TextOverflow.ellipsis,
+                      overflow: TextOverflow.ellipsis, // Very long text ...
                       style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
                     const SizedBox(height: 12),
                     Row(
@@ -79,9 +85,9 @@ class MealItem extends StatelessWidget {
                         MealItemTrait(
                           icon: Icons.attach_money,
                           label: affordabilityText,
-                        ),
+                        )
                       ],
-                    )
+                    ),
                   ],
                 ),
               ),
